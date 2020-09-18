207a1d239d47cf725c424f6a08840e2862533b7a
annkamsk <kamsk@google.com>
2020-09-18 15:43:49 +0200

    Working interceptor with not-deprecated API

--- src/server.js
+++ src/server.js
@@ -17,50 +17,50 @@ function transform(source) {
 async function intercept(page, patterns, transform) {
     const client = await page.target().createCDPSession();
 
-    await client.send('Network.enable');
-
-    await client.send('Network.setRequestInterception', {
+    await client.send('Fetch.enable', {
         patterns: [
-            {urlPattern: '*', resourceType: 'Script', interceptionStage: 'HeadersReceived'},
-            {urlPattern: '*', resourceType: 'Document', interceptionStage: 'HeadersReceived'},
-            {urlPattern: '*', resourceType: 'CSPViolationReport', interceptionStage: 'HeadersReceived'},
+            {requestStage: 'Response', resourceType: 'Document'},
+            {requestStage: 'Response', resourceType: 'Script'},
         ]
     });
 
-    client.on('Network.requestIntercepted', async ({ interceptionId, request, responseHeaders, resourceType }) => {
-        console.log(`Intercepted ${request.url} {interception id: ${interceptionId}}`);
+    client.on('Fetch.requestPaused', async event => {
+        const { requestId, resourceType, request, responseHeaders } = event;
+        // console.log(`Intercepted ${request.url} {interception id: ${requestId}}`);
 
-        const response = await client.send('Network.getResponseBodyForInterception',{ interceptionId });
+        const response = await client.send('Fetch.getResponseBody',{ requestId });
 
-        const contentTypeHeader = Object.keys(responseHeaders).find(k => k.toLowerCase() === 'content-type');
-        let newBody, contentType = responseHeaders[contentTypeHeader];
+        const contentTypeHeader = responseHeaders.find(header => header.name === 'Content-Type');
+        let newBody, contentType = contentTypeHeader.value;
 
         if (requestCache.has(response.body)) {
             newBody = requestCache.get(response.body);
         } else {
             const bodyData = response.base64Encoded ? atob(response.body) : response.body;
             try {
-                newBody = transform(bodyData, { parser: 'babel' });
+                if (contentType === 'text/javascript') {
+                    newBody = transform(bodyData, { parser: 'babel' });
+                } else {
+                    newBody = bodyData;
+                }
             } catch(e) {
-                console.log(`Failed to process ${request.url} {interception id: ${interceptionId}}: ${e}`);
+                console.log(`Failed to process ${request.url} {interception id: ${requestId}}: ${e}`);
                 newBody = bodyData
             }
 
             requestCache.set(response.body, newBody);
         }
 
-        const newHeaders = [
-            'Date: ' + (new Date()).toUTCString(),
-            'Connection: closed',
-            'Content-Length: ' + newBody.length,
-            'Content-Type: ' + contentType,
-            'Content-Security-Policy-Report-Only: require-trusted-types-for \'script\';'
-        ];
-
-        console.log(`Continuing interception ${interceptionId}`)
-        client.send('Network.continueInterceptedRequest', {
-            interceptionId,
-            rawResponse: btoa('HTTP/1.1 200 OK' + '\r\n' + newHeaders.join('\r\n') + '\r\n\r\n' + newBody)
+        const newHeaders = responseHeaders;
+        newHeaders.push({
+            name: 'Content-Security-Policy-Report-Only', value: 'require-trusted-types-for \'script\';'
+        });
+        // console.log(`Continuing interception ${requestId}`)
+        await client.send('Fetch.fulfillRequest', {
+            requestId: requestId,
+            responseHeaders: newHeaders,
+            responseCode: 200,
+            body: response.body
         });
     });
 }
