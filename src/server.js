0e27adcb785b8765b03bb2f145c679eca72d8be7
annkamsk <kamsk@google.com>
2020-09-18 15:50:05 +0200

    Logging the CSP violations

--- src/server.js
+++ src/server.js
@@ -21,39 +21,21 @@ async function intercept(page, patterns, transform) {
         patterns: [
             {requestStage: 'Response', resourceType: 'Document'},
             {requestStage: 'Response', resourceType: 'Script'},
+            {requestStage: 'Response', resourceType: 'CSPViolationReport'},
         ]
     });
 
     client.on('Fetch.requestPaused', async event => {
         const { requestId, resourceType, request, responseHeaders } = event;
         // console.log(`Intercepted ${request.url} {interception id: ${requestId}}`);
-
-        const response = await client.send('Fetch.getResponseBody',{ requestId });
-
-        const contentTypeHeader = responseHeaders.find(header => header.name === 'Content-Type');
-        let newBody, contentType = contentTypeHeader.value;
-
-        if (requestCache.has(response.body)) {
-            newBody = requestCache.get(response.body);
-        } else {
-            const bodyData = response.base64Encoded ? atob(response.body) : response.body;
-            try {
-                if (contentType === 'text/javascript') {
-                    newBody = transform(bodyData, { parser: 'babel' });
-                } else {
-                    newBody = bodyData;
-                }
-            } catch(e) {
-                console.log(`Failed to process ${request.url} {interception id: ${requestId}}: ${e}`);
-                newBody = bodyData
-            }
-
-            requestCache.set(response.body, newBody);
+        if (resourceType === 'CSPViolationReport') {
+            console.error(event);
         }
+        const response = await client.send('Fetch.getResponseBody',{ requestId });
 
         const newHeaders = responseHeaders;
         newHeaders.push({
-            name: 'Content-Security-Policy-Report-Only', value: 'require-trusted-types-for \'script\';'
+            name: 'Content-Security-Policy-Report-Only', value: 'require-trusted-types-for \'script\'; report-uri http://127.0.0.1:8080'
         });
         // console.log(`Continuing interception ${requestId}`)
         await client.send('Fetch.fulfillRequest', {
